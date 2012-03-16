8318e13dc9a92a4b308450f59737cd66a9a0fe4a
Danesh M <daneshm90@gmail.com>
2012-03-15 23:16:29 -0400

    Apn settings fixes / code maintainence
    Depends on : http://review.cyanogenmod.com/#change,13743
    
    Cleanup unecessary resources and use global ones
      - This fixes the duplicate id issue currently causing apn's to be un-editable
    
    Bring the ui aspect of the preference up to speed with ics
     - Use ics highlight / layout...etc
     - Use fragments
    
    Patchset 5 : Remove theme for ApnEditor
                 Use static access for variables
    Patchset 6 : Launch apn editor internally as a fragment
    	     Replace deprecated managedQuery with CursorLoader
    
    Change-Id: I069415b29846f30665ce76e52f04115d1b65e455
    
    Conflicts:
    	AndroidManifest.xml
    	res/layout/apn_preference_layout.xml
    	src/com/android/settings/ApnEditor.java
    	src/com/android/settings/ApnSettings.java
    	src/com/android/settings/Settings.java
    	src/com/android/settings/profiles/ProfilesPreference.java
    
    Updated with CM10.2 source!

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -837,6 +837,8 @@ public class Settings extends PreferenceActivity
     public static class TextToSpeechSettingsActivity extends Settings { /* empty */ }
     public static class AndroidBeamSettingsActivity extends Settings { /* empty */ }
     public static class WifiDisplaySettingsActivity extends Settings { /* empty */ }
+    public static class ApnSettingsActivity extends Settings { /* empty */ }
+    public static class ApnEditorActivity extends Settings { /* empty */ }
     public static class DreamSettingsActivity extends Settings { /* empty */ }
     public static class NotificationStationActivity extends Settings { /* empty */ }
     public static class UserSettingsActivity extends Settings { /* empty */ }
