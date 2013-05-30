656a700347108e33ed7c022e0c4511faf4569fb9
BigBrother1984 <stevewatersy@gmail.com>
2013-05-30 13:23:27 +0200

    Pie: set angle default to 12° (2/2)

--- src/com/android/settings/paranoid/Toolbar.java
+++ src/com/android/settings/paranoid/Toolbar.java
@@ -181,7 +181,7 @@ public class Toolbar extends SettingsPreferenceFragment
 
         mPieAngle = (ListPreference) prefSet.findPreference(PIE_ANGLE);
         int pieAngle = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.PIE_ANGLE, 0);
+                Settings.System.PIE_ANGLE, 12);
         mPieAngle.setValue(String.valueOf(pieAngle));
         mPieAngle.setOnPreferenceChangeListener(this);
 
