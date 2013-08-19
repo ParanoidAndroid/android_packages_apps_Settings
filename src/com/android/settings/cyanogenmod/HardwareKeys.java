40e253430684df3d7ef3805f968fe81713bd6728
Michael Bestas <mikeioannina@gmail.com>
2013-08-19 15:23:40 +0300

    Fix removal of backlight settings
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: I118e667b306a5a1e50d593f3f18828b7a921a511

--- src/com/android/settings/cyanogenmod/HardwareKeys.java
+++ src/com/android/settings/cyanogenmod/HardwareKeys.java
@@ -204,11 +204,11 @@ public class HardwareKeys extends SettingsPreferenceFragment implements
 
         if (ButtonBacklightBrightness.isSupported() || KeyboardBacklightBrightness.isSupported()) {
             if (!ButtonBacklightBrightness.isSupported()) {
-                removePreference(KEY_BUTTON_BACKLIGHT);
+                backlightCategory.removePreference(findPreference(KEY_BUTTON_BACKLIGHT));
             }
 
             if (!KeyboardBacklightBrightness.isSupported()) {
-                removePreference(KEY_KEYBOARD_BACKLIGHT);
+                backlightCategory.removePreference(findPreference(KEY_KEYBOARD_BACKLIGHT));
             }
         } else {
             prefScreen.removePreference(backlightCategory);
